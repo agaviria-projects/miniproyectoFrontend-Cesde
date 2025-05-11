c927b67c2effe3b4b2b914360614c99d1d589e2a
Alejandro <agaviria1408@gmail.com>
2025-05-11 15:25:57 -0500

    Se personaliza el proyecto integrador con el nombre de Alejandro Gaviria

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import PurchaseForm from './components/PurchaseForm';
 function App() {
   return (
     <div className="container">
-      <h1>Proyecto “Customer & Purchase”</h1>
-      <h2>CESDE 2025</h2>
+       <h1>Sistema de Registro Comercial</h1>
+       <h2>Proyecto Integrador - Alejandro Gaviria</h2>
+       <h3>CESDE 2025</h3>
       <CustomerForm />
       <hr />
       <PurchaseForm />
