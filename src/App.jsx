006c161edbcdcfdc424bec8f0688dbd25ac6b56b
Alejandro <agaviria1408@gmail.com>
2025-05-11 15:31:28 -0500

    Se ajusta el autor del proyecto nombre de Alejandro Gaviria

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import PurchaseForm from './components/PurchaseForm';
 function App() {
   return (
     <div className="container">
-       <h1>Sistema de Registro Comercial</h1>
-       <h2>Proyecto Integrador - Alejandro Gaviria</h2>
-       <h3>CESDE 2025</h3>
+      <h1>Sistema de Registro Comercial</h1>
+      <h2 className="autor">Proyecto Integrador - Alejandro Gaviria</h2>
+      <h3>CESDE 2025</h3>
       <CustomerForm />
       <hr />
       <PurchaseForm />
