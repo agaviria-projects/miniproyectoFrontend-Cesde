46ebd28e301a6e99daa52f6b3e781d79c458ac03
Alejandro <agaviria1408@gmail.com>
2025-05-11 16:07:16 -0500

    Se reajustan los titulos de la app

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ function App() {
   return (
     <div className="container">
       <h1>Sistema de Registro Comercial</h1>
-      {/*<h2 className="seccion-titulo">Registrar Cliente</h2>*/}
+      <h2 className="seccion-titulo">Registrar Cliente</h2>
       <CustomerForm />
       <hr />
-      {/*<h2 className="seccion-titulo">Registrar Compra</h2>*/}
+      <h2 className="seccion-titulo">Registrar Compra</h2>
       <PurchaseForm />
 
       {/* Pie de página personalizado */}
